6aa522257531ca124570458749b9f4bf56031fa6
fedihor <fedihor@mail.ua>
2016-08-21 17:59:37 +0300

    Start

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/ownstyle.css',
+        'css/template/style.css',
+        'css/template/red.css',
+        'css/template/responsive.css',
+        'css/template/font-awesome.min.css',
+        'css/template/slicknav.css',
+    ];
+    public $js = [
+        'js/jquery-2.1.4.min.js',
+        'js/jquery.migrate.js',
+        'js/modernizrr.js',
+        'js/bootstrap.min.js',
+        'js/jquery.fitvids.js',
+        'js/owl.carousel.min.js',
+        'js/nivo-lightbox.min.js',
+        'js/jquery.isotope.min.js',
+        'js/jquery.appear.js',
+        'js/count-to.js',
+        'js/jquery.textillate.js',
+        'js/jquery.lettering.js',
+        'js/jquery.easypiechart.min.js',
+        'js/jquery.nicescroll.min.js',
+        'js/jquery.parallax.js',
+        'js/jquery.slicknav.js', 
+        'js/script.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
